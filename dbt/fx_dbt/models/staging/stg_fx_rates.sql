46a5bce2435f0cbb2fc287c300b6a7bd2d7b3e7b
Yurii Kiktenko <233724630+YuriiKiktenko@users.noreply.github.com>
2026-03-04 19:12:39 +0100

    refactor: add tests and start_date to stage

--- dbt/fx_dbt/models/staging/stg_fx_rates.sql
+++ dbt/fx_dbt/models/staging/stg_fx_rates.sql
@@ -3,7 +3,9 @@
 select
   rate_date,
   date_trunc(rate_date, month) as rate_month,
-  "{{ var('fx_base_currency', 'EUR') }}" as base_currency,
+  "{{ var('fx_base_currency') }}" as base_currency,
   upper(trim(currency)) as currency,
   rate
-from {{ source('fx_core', 'fx_core_daily') }} where rate_date >= "2026-01-01"
+from {{ source('fx_core', 'fx_core_daily') }}
+where rate_date >= "{{ var('fx_start_date', '2026-01-01') }}"
+  and currency in unnest({{ var('fx_required_currencies') }})
--- dbt/fx_dbt/models/staging/stg_fx_rates_wide.sql
+++ dbt/fx_dbt/models/staging/stg_fx_rates_wide.sql
@@ -1,18 +1,14 @@
 {{ config(materialized='view') }}
 
-with src as (
-  select
-    rate_date,
-    currency,
-    rate
-  from {{ ref('stg_fx_rates') }}
-  where currency in unnest({{ var('fx_required_currencies') }})
-)
-
 select
   rate_date,
   {% for ccy in var('fx_required_currencies') %}
+  {% if ccy == var('fx_base_currency') %}
+  cast(1.0 as numeric) as rate_{{ ccy|lower }}{% if not loop.last %},{% endif %}
+  {% else %}
   max(if(currency = '{{ ccy }}', rate, null)) as rate_{{ ccy|lower }}{% if not loop.last %},{% endif %}
+  {% endif %}
   {% endfor %}
-from src
+from {{ ref('stg_fx_rates') }}
 group by 1
+order by 1 desc
